85dc269ebcb0d6847ed5b521a799846cf27f2ef8
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-26 11:58:09 -0500

    hasAZero first attempt

--- some-every.js
+++ some-every.js
@@ -20,7 +20,13 @@ Examples:
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+  splitNum = num.split("");
+
+  splitNum.some(function (val) {
+    return val === 0;
+  });
+}
 
 /*
 Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
